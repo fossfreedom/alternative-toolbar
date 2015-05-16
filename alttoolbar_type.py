f623201851a60a549b4ca70e4a4adc8fb2c7d34b
fossfreedom <foss.freedom@gmail.com>
2015-05-16 20:38:00 +0100

    copyright stuff in module

--- alttoolbar_type.py
+++ alttoolbar_type.py
@@ -1,3 +1,21 @@
+# -*- Mode: python; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; -*-
+#
+# Copyright (C) 2015 - fossfreedom
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
+
 from datetime import datetime, date
 from gi.repository import Gtk
 from gi.repository import GObject
@@ -131,7 +149,7 @@ class AltToolbarBase(GObject.Object):
     def reset_toolbar(self, page):
         '''
            whenever a source changes this resets the toolbar to reflect the changed source
-           :param RBDisplayPage
+           :param page - RBDisplayPage
         '''
         if not page:
             return
