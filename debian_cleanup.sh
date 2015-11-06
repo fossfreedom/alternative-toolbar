54155f425c4c1c04bebb83df84bab2531ddabf7b
fossfreedom <fossfreedom@gmail.com>
2015-11-06 16:47:34 +0000

    prepare for split

--- debian_cleanup.sh
+++
@@ -1,9 +0,0 @@
-# cleanup script to tidyup various elements of the source tree that 
-# should not be in the debian package
-
-rm -rf .git
-rm -rf .idea
-rm -rf gentoo
-rm img/rb-symbolic-icons.tar.gz
-rm install.sh
-rm debian_cleanup.sh
